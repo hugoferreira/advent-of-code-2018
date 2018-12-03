c119f6ff2d53a815d0ef413ecbe17f96bf3ab7b8
Hugo Ferreira <bytter@gmail.com>
2018-12-03 21:30:22 +0100

    Simplified Part 1. Solved Part 2.

--- src/3/solution-part-1.js
+++ src/3/solution-part-1.js
@@ -2,17 +2,15 @@ const readFileSync = require('fs').readFileSync
 const _ = require('lodash/fp')
 
 const input = readFileSync('src/3/input.txt').toString().split('\n')
-const data = _.fromPairs(input.map(line => {
-    const [ id, left, top, width, height ] = line.match(/\d+/g).map(x => +x) // thank you, javascript -_-'
-    return [ id, { left, top, width, height } ]
-}))
+const data = input.map(l => l.match(/\d+/g).map(x => parseInt(x, 10)))
 
-const fill = (s) => _.range(s.top, s.height + s.top).flatMap(y =>
-                    _.range(s.left, s.width + s.left).map(x => [x, y]))
+const fill = (s) => _.range(s[2], s[4] + s[2]).flatMap(y =>
+                    _.range(s[1], s[3] + s[1]).map(x => [x, y]))
 
-const grid = _.pipe(_.flatMap(fill),
-                    _.groupBy(_.identity),
-                    _.mapValues(_.size),
-                    _.toPairs)(data)
+const solution = _.pipe(_.flatMap(fill),
+                        _.groupBy(_.identity),
+                        _.values,
+                        _.map(_.size),
+                        _.countBy(p => p >= 2))(data)
 
-console.debug(_.countBy(p => p[1] >= 2)(grid))
+console.debug(solution.true)
---
+++ src/3/solution-part-2.js
@@ -0,0 +1,13 @@
+const readFileSync = require('fs').readFileSync
+const _ = require('lodash/fp')
+
+const input = readFileSync('src/3/input.txt').toString().split('\n')
+const data = input.map(line => {
+    const [ id, x0, y0, width, height ] = line.match(/\d+/g).map(x => parseInt(x, 10))
+    return { id, x0, y0, x1: x0 + width - 1, y1: y0 + height - 1 }
+})
+
+const bbIntersect = (a, b) => !(b.x0 > a.x1 || b.x1 < a.x0 || b.y0 > a.y1 || b.y1 < a.y0)
+const dontIntersect = data.filter(a => data.every(b => a.id === b.id || !bbIntersect(a, b)))
+
+console.debug(dontIntersect[0].id) //?
