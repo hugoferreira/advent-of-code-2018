8bf60a2bfcf347338e73c096ec7ce1fd06110eba
Hugo Ferreira <bytter@gmail.com>
2018-12-07 00:46:51 +0000

    Code cleanup

--- src/6/solution-part-2.js
+++ src/6/solution-part-2.js
@@ -4,12 +4,12 @@ const input = require('fs').readFileSync('src/6/input.txt').toString().split('\n
 const data = input.map(line => line.match(/\d+/g).map(x => parseInt(x, 10)))
 const threshold = 10000
 
-const bb = { x0: _.min(data.map(_.first)), y0: _.min(data.map(_.last)),
-             x1: _.max(data.map(_.first)), y1: _.max(data.map(_.last)) }
+const [x0, x1] = [_.min, _.max].map(f => f(data.map(_.first)))
+const [y0, y1] = [_.min, _.max].map(f => f(data.map(_.last)))
 
 const dist = (x, y) => (p) => Math.abs(p[0] - x) + Math.abs(p[1] - y)
-const grid = _.range(bb.y0, bb.y1 + 1).map(y =>
-                _.range(bb.x0, bb.x1 + 1).map(x =>
+const grid = _.range(y0, y1 + 1).map(y =>
+                _.range(x0, x1 + 1).map(x =>
                     _.sum(data.map(dist(x, y))) < threshold))
 
-console.debug(_.countBy(_.identity)(_.flatten(grid)).true)
+console.log(_.countBy(_.identity)(_.flatten(grid)).true)
